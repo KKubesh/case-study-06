2974271ba376ce4a303e8f5f84a238f88e9ce558
Kam Kubesh <kkubesh@cliqstudios.com>
2019-03-11 23:28:20 -0500

    refactored product highlights and adjusted styling in App.css to better reflect design guidelines for items inside the ProductReview section

--- src/components/ProductDetails/Highlights.js
+++ src/components/ProductDetails/Highlights.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 
 class Highlights extends Component {
+    state = {
+        details: [
+            "Wattage Output: 1100 Watts",
+            "Number of Speeds: 3",
+            "Capacity (volume): 72.0 Oz",
+            "Appliance Capabilities: Blends",
+            "Includes: Travel Lid",
+            "Material: Plastic",
+            "Finish: Painted",
+            "Metal Finish: Chrome",
+            "Safety and Security Features: Non-Slip Base",
+            "Care and Cleaning: Easy-To-Clean, Dishwasher Safe Parts"
+        ]
+    }
     render () {
+        let detailItems = this.state.details.map(item => {
+            return (
+                <li><span>{item}</span></li>
+            )
+        })
         return (
             <div className="product-highlights" data-testid="product-highlights">
                 <h2 data-testid="highlights-title">product highlights</h2>
                 <ul data-testid="highlights-details">
-                    <li>Wattage Output: 1100 Watts</li>
-                    <li>Number of Speeds: 3</li>
-                    <li>Capacity (volume): 72.0 Oz</li>
-                    <li>Appliance Capabilities: Blends</li>
-                    <li>Includes: Travel Lid</li>
-                    <li>Material: Plastic</li>
-                    <li>Finish: Painted</li>
-                    <li>Metal Finish: Chrome</li>
-                    <li>Safety and Security Features: Non-Slip Base</li>
-                    <li>Care and Cleaning: Easy-To-Clean, Dishwasher Safe Parts</li>
+                    {detailItems}
                 </ul>
             </div>
         )
